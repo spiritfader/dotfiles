11b91f3a648b8dd0b8acc6522c76bdb8b84500cb
spiritfader <spiritfader@proton.me>
2023-03-29 13:50:29 -0400

    add scripts

---
+++ .config/scripts/networkInterfaceBandwidth.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+INTERVAL="1"  # update interval in seconds
+
+if [ -z "$1" ]; then
+        echo
+        echo usage: "$0" network-interface
+        echo
+        echo e.g. "$0" eth0
+        echo
+        exit
+fi
+
+while true
+do
+        R1=$(cat /sys/class/net/"$1"/statistics/rx_bytes)
+        T1=$(cat /sys/class/net/"$1"/statistics/tx_bytes)
+        sleep $INTERVAL
+        R2=$(cat /sys/class/net/"$1"/statistics/rx_bytes)
+        T2=$(cat /sys/class/net/"$1"/statistics/tx_bytes)
+        TBPS=$((T2 - T1))
+        RBPS=$((R2 - R1))
+        TKBPS=$((TBPS / 1024))
+        RKBPS=$((RBPS / 1024))
+        echo "TX $1: $TKBPS kB/s RX $1: $RKBPS kB/s"
+done
---
+++ .config/scripts/networkInterfacePPS.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+INTERVAL="1"  # update interval in seconds
+
+if [ -z "$1" ]; then
+        echo
+        echo usage: "$0" network-interface
+        echo
+        echo e.g. "$0" eth0
+        echo
+        echo shows packets-per-second
+        exit
+fi
+
+while true
+do
+        R1=$(cat /sys/class/net/"$1"/statistics/rx_packets)
+        T1=$(cat /sys/class/net/"$1"/statistics/tx_packets)
+        sleep $INTERVAL
+        R2=$(cat /sys/class/net/"$1"/statistics/rx_packets)
+        T2=$(cat /sys/class/net/"$1"/statistics/tx_packets)
+        TXPPS=$((T2 - T1))
+        RXPPS=$((R2 - R1))
+        echo "TX $1: $TXPPS pkts/s RX $1: $RXPPS pkts/s"
+done
