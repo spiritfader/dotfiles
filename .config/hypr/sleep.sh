0271f2786bd54b6a71a2a76d966d6662909a40bf
spiritfader <spiritfader@proton.me>
2024-03-26 12:31:39 -0400

    add zathura config

--- .config/hypr/sleep.sh
+++ .config/hypr/sleep.sh
@@ -1,5 +1,5 @@
 swayidle -w timeout 300 'brightnessctl set 0%' resume 'brightnessctl set 80%' \
             timeout 310 'wff-lock.sh' \
             timeout 600 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on' \
-            timeout 1200 'systemctl suspend' \
+            timeout 1200 'hyprctl dispatch dpms on && systemctl suspend' \
             before-sleep 'wff-lock.sh' &
--- .local/bin/wff-lock.sh
+++ .local/bin/wff-lock.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-pngfile=/tmp/lock.png
-pngfileedit=/tmp/lockedit.png
-grim -t png $pngfile
-ffmpeg -loglevel quiet -y -i "$pngfile" -vf scale=10:540,scale=1920:1080 $pngfileedit
-swaylock -fFeli $pngfileedit
-rm "$pngfile" "$pngfileedit"
+capture=/tmp/lock.png
+lockscreen=/tmp/lockedit.png
+grim -t png $capture; ffmpeg -loglevel quiet -y -i "$capture" -vf scale=10:540,scale=1920:1080 "$lockscreen"
+swaylock -fFeli $lockscreen
+rm "$capture" "$lockscreen"
